7ec237ec0b2d9d281e9dcc672c06bc4cdddea2f5
= <ibrahim@mercadolar.com>
2017-01-08 00:45:12 -0500

    buildfile

--- Build.ps1
+++ Build.ps1
@@ -2,8 +2,6 @@ if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
 EnsurePsbuildInstalled
 
-Invoke-MSBuild
-
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
