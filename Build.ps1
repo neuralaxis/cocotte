3b5c19b79c8e04dd5b2fd48a2c5d6b07f5a2a2ce
= <ibrahim@mercadolar.com>
2017-01-08 00:51:52 -0500

    buildfile

--- Build.ps1
+++ Build.ps1
@@ -5,4 +5,5 @@ EnsurePsbuildInstalled
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
-exec { & dotnet pack Cocotte\Cocotte.csproj -c Release -o ..\artifacts --version-suffix $revision-prerelease }  
+& dotnet restore Cocotte\Cocotte.csproj
+& dotnet pack Cocotte\Cocotte.csproj -c Release -o ..\artifacts --version-suffix $revision-prerelease 
